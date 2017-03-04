23e8a14aa3bd770f40ec6b7e49fc9fc08cec1088
Yue <ren@mathematik.uni-kl.de>
2017-03-04 20:53:30 +0100

    chg: generatePermutationGroup -> permutationGroup

--- Singular/dyn_modules/tropical/symmetry.h
+++ Singular/dyn_modules/tropical/symmetry.h
@@ -7,7 +7,7 @@
 #include <gfanlib/gfanlib.h>
 
 std::vector<int> composePermutations(const std::vector<int> &sigma, const std::vector<int> &tau);
-std::set<std::vector<int> > generatePermutationGroup(const std::set<std::vector<int> > &generators);
+std::set<std::vector<int> > permutationGroup(const std::set<std::vector<int> > &generators);
 
 gfan::ZVector applyPermutation(const gfan::ZVector &w, const std::vector<int> &g);
 gfan::ZVector minimalRepresentative(const gfan::ZVector &w, const std::set<std::vector<int> > &G);
